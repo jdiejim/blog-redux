9582e8b37528c67d539a3cf4b37fa7385301a5f4
Juan Diego Jimenez <jdiejim@gmail.com>
2017-06-18 10:14:44 -0600

    add redux-form packages and setup

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -1,26 +1,66 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import styled from 'styled-components';
+
+const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  input {
+    padding: 10px;
+    border: 1px solid #EDEDED;
+    border-radius: 10px;
+    &:focus {
+      outline: none;
+      border: 1px solid #00b2ff;
+    }
+  }
+  label {
+    margin: 10px 0;
+    font-size: 20px;
+    text-transform: capitalize;
+  }
+`
 
 class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <InputWrapper>
+        <label>{field.label}</label>
+        <input
+          type="text"
+          {...field.input}
+        />
+      </InputWrapper>
+    )
+  }
+
   render() {
     return (
       <form>
-        <label>
-          Title:
-          <input />
-        </label>
-        <label>
-          Categories:
-          <input />
-        </label>
-        <label>
-          Contents:
-          <input />
-        </label>
-        <button>Save</button>
-        <button>Cancel</button>
+        <div>
+          <lable></lable>
+          <Field
+            name="title"
+            label="title"
+            component={this.renderField}
+           />
+          <Field
+            name="categories"
+            label="categories"
+            component={this.renderField}
+           />
+          <Field
+            name="content"
+            label="post content"
+            component={this.renderField}
+           />
+        </div>
       </form>
     )
   }
 }
 
-export default  PostsNew;
+const validate = values => {
+}
+
+export default reduxForm({ validate, form: 'PostsNewForm' })(PostsNew);
